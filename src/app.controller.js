b9ec52cfbc9838a199b08027068a44226faf848a
Mazen-Mokhtar <mazenmokhtar.iq@gmail.com>
2025-03-11 15:00:14 +0200

    fix url

--- src/app.controller.js
+++ src/app.controller.js
@@ -6,15 +6,22 @@ import { globalError, urlError } from "./utils/index.js"
 import cors from "cors"
 
 export const bootstrap = (app, express) => {
-    app.use(cors({
-        origin: 'http://localhost:3000', // Allow only your frontend origin
-        methods: ['GET', 'POST', 'PUT', 'DELETE',"PATCH"], // Specify allowed methods
-        allowedHeaders: ['Content-Type'], // Specify allowed headers
-      }));
-    conctionDB()
-    app.use(express.json())
-    app.use("/request", requestController)
-    app.use("/user", userController)
-    app.all("*", urlError);
-    app.use(globalError)
+  app.use(cors({
+    origin: '*', // Allow only your frontend origin
+    methods: ['GET', 'POST', 'PUT', 'DELETE', "PATCH"], // Specify allowed methods
+    allowedHeaders: ['Content-Type'], // Specify allowed headers
+  }));
+
+  conctionDB()
+
+  app.use(express.json())
+  app.use(express.urlencoded({ extended: true }));
+  app.use((req, res, next) => {
+    console.log(`📌 Received ${req.method} request on ${req.url}`);
+    next();
+  });
+  app.use("/request", requestController)
+  app.use("/user", userController)
+  app.all("*", urlError);
+  app.use(globalError)
 }
